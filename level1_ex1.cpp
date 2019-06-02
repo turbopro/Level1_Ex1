fe8ebe1de84fd7c1b4e62d9f565a0ef1b80c577d
Vernon Woolford <wool@un.org>
2019-06-01 23:23:38 -0400

    Add project files.

---
+++ level1_ex1.cpp
@@ -0,0 +1,18 @@
+/*
+level1_ex1.cpp -- file with code for Level 1, Ex 1
+File with the main() function
+First C-program for CPFE (C++ Programming for Financial Engineering)
+Only C code
+*/
+
+#include <stdio.h>			// Header file for C library standard input/output
+
+int main(void)
+{
+	/* output text to the screen: use printf() */
+	printf("My first C-program\n");
+	printf("is a fact!\n");
+	printf("Good, isn't it?\n");
+
+	return 0;
+}
